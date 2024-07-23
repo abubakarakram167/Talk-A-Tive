2e9b3e238188e913c918617a87a56bae196e3c99
abubakar167 <abubakarakram167@gmail.com>
2024-07-24 00:52:58 +0500

    Add user  search endpoint and authentication middleware before request made.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
 	userRegisteration,
 	authUser,
+	allUsers,
 } = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(userRegisteration);
+router.route("/").post(userRegisteration).get(protect, allUsers);
 router.post("/login", authUser);
 
 module.exports = router;
