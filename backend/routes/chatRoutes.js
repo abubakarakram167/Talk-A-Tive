7dd18c4a64402c8cbb1365a3211511a2b748ef54
abubakar167 <abubakarakram167@gmail.com>
2024-07-24 21:53:08 +0500

    Add new user to current group chat and remove user to current group chat.

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,7 +4,11 @@ const {
 	fetchChats,
 	createGroupChat,
 	renameGroup,
+	removeFromGroup,
+	addToGroup,
 } = require("../controllers/chatController");
+
+//Authentication Middleware.
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
@@ -13,7 +17,7 @@ router.route("/").get(protect, fetchChats);
 router.route("/").post(protect, accessChat);
 router.route("/creategroupchat").post(protect, createGroupChat);
 router.route("/renameGroup").put(protect, renameGroup);
-//  router.route("/addToGroup").post(protect, addToGroup);
-//  router.route("/removeFromGroup").put(protect, removeFromGroup);
+router.route("/addToGroup").post(protect, addToGroup);
+router.route("/removeFromGroup").put(protect, removeFromGroup);
 
 module.exports = router;
